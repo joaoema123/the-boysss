3e3c9d915703f97e33013dc7fe535538ca834b31
joaoema123 <joao.emanuel.inv@gmail.com>
2025-11-18 20:06:40 -0300

    Add files via upload

--- theboys.c
+++ theboys.c
@@ -8,7 +8,45 @@
 #include "fprio.h"
 #include "conjunto.h"
 // seus #defines vão aqui
+struct heroi 
+{
+  int ID; // número que identifica o herói
+  struct cjto_t habilidades; // conjunto de habilidades que o herói possui
+  int paciencia; // paciência do herói
+  int velocidade; // velocidade de deslocamento do herói
+  int experiencia; // número de missões que o heróis já participou
+  int base; //ID da base onde o herói se encontra
+} ;
 
+struct base
+{
+  int ID; // número inteiro que identifica a base
+  int lotaçao; // número máximo de heróis na base
+  int *presentes; // ponteiro para vetor que contem todos heróis presentes na base
+  struct fila_nodo_t espera; // fila onde os heróis esperam para entrar na base
+  int localizacao [2]; // par ordenado que indica as coordenadas da base
+} ;
+
+struct missao 
+{
+  int  ID; // número inteiro que identifica a missão
+  struct cjto_t habilidades; // conjunto de habilidades necessárias para realizar a missão
+  int localizacao [2]; // par ordenado que indica as coordenadas da missão
+} ;
+
+struct mundo 
+{
+  int NHerois; // número total de heróis no mundo
+  int *herois; // ponteiro para vetor que contem todos heróis
+  int NBases; // número total de bases no mundo
+  int *bases; // ponteiro para vetor que contem todas bases
+  int NMissoes; // número total e missões a cumprir
+  int *missoes; // ponteiro para vetor que contem todas missões a se cumprir
+  int NHabilidades; // número de habilidades distintas que existem no mundo
+  int NCompostosV; // número de compostos V disponíveis
+  int TamanhoMundo [2]; // coordenadas máximas do plano cartesiano que representam o tamanho do mundo
+  int relogio; // indica o tempo atual do mundo
+} ; 
 // minimize o uso de variáveis globais
 
 // programa principal
